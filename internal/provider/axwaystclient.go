43e059b8ed5fe8759661d0a4da1374eab08a7887
Travis Stratton <stra0400@umn.edu>
2025-06-25 14:20:54 -0500

    refactor: Modify auth to grab csrfToken for subsequent http requests

--- internal/provider/axwaystclient.go
+++ internal/provider/axwaystclient.go
@@ -10,13 +10,12 @@ import (
 )
 
 type AxwaySTClient struct {
-	client    *http.Client
 	jarclient *http.Client
 	endpoint  string
-	auth      string
+	csrfToken string
 }
 
-// A wrapper for http.NewRequestWithContext() that prepends axwayst endpoint to URL & sets authorization
+// A wrapper for http.NewRequestWithContext() that prepends axwayst endpoint to URL & sets
 // headers and then makes the actual http request.
 func (c *AxwaySTClient) GenericAPIRequest(ctx context.Context, method, url string, requestBody any, successCodes []int) (responseBody []byte, statusCode int, errorMessage error) {
 	url = c.endpoint + url
@@ -39,27 +38,13 @@ func (c *AxwaySTClient) GenericAPIRequest(ctx context.Context, method, url strin
 		return
 	}
 	httpReq.Header.Add("Content-Type", "application/json")
+	httpReq.Header.Add("csrfToken", c.csrfToken)
+	httpReq.Header.Add("Referer", "terraform")
 
-	var httpResp *http.Response
-
-	// Get requests work with using the JAR token, but other types of requests fail with Not-valid CSRF prevention token error
-
-	if method == http.MethodGet {
-		httpReq.Header.Add("Referer", "terraform")
-
-		httpResp, err = c.jarclient.Do(httpReq)
-		if err != nil {
-			errorMessage = fmt.Errorf("error doing http request: %v", err)
-			return
-		}
-	} else {
-		httpReq.Header.Add("Authorization", c.auth)
-
-		httpResp, err = c.client.Do(httpReq)
-		if err != nil {
-			errorMessage = fmt.Errorf("error doing http request: %v", err)
-			return
-		}
+	httpResp, err := c.jarclient.Do(httpReq)
+	if err != nil {
+		errorMessage = fmt.Errorf("error doing http request: %v", err)
+		return
 	}
 
 	var success bool
